198d67f9cbd92f2a4102faf4a460689d94d1b27e
elizzybeth <elizzybeth@gmail.com>
2018-05-23 04:21:59 -0500

    Can now get one site full of frequency counts

--- js/accumulator.js
+++ js/accumulator.js
@@ -1,29 +1,54 @@
 var MongoClient = require('mongodb').MongoClient;
 
 // find unique list of sites
-function findSites(articles){
-  articles.distinct("site",(function(err,sites){
-    console.log(sites);
-  }))
+async function findSites(articles){
+  var sites = await articles.distinct("site");
+  return sites;
 }
+
+// for each site, find all the frequencyObjects for that site
+async function getFrequencyObjectBySite(articles){
+  var sites = await findSites(articles);
+  console.log(sites);
+  var results = {};
+  sites.forEach(async function(site){
+    console.log(site);
+    results[site] = await getOneSiteFrequencyObject(site, articles);
+    process.exit();
+  });
+  return results;
+}
+
+async function getOneSiteFrequencyObject(site, articles) {
+  var FOCursor = await articles.find({"site": site},{"frequency": true});
+  var FOArray = await FOCursor.toArray();
+  var accumulator = {};
+  var wordCount = 0;
+  FOArray.forEach(function({frequency}){
+    Object.keys(frequency).forEach(function(word){
+      if(accumulator.hasOwnProperty(word)){
+        accumulator[word] += frequency[word];
+      } else {
+        accumulator[word] = frequency[word];
+      }
+      wordCount += frequency[word];
+    });
+  });
+  console.log(accumulator);
+  console.log(wordCount);
+  return 1;
+}
+
 
 /*
 
-// for each site, find all the frequencyObjects for that site
+
 // reduce the frequencyObjects
 
 // from [one frequency object] to [meta frequency object]
 
 
-function frequencyObjectToMeta(metaFrequencyObject, frequencyObject){
-  Object.keys(frequencyObject).forEach(function(word){
-    if(metaFrequencyObject.hasOwnProperty(word)){
-      metaFrequencyObject[word] += frequencyObject[word];
-    } else {
-      metaFrequencyObject[word] = frequencyObject[word];
-    }
-  });
-}
+
 
 // imagine we have array called articles that's an array of frequencyObject-having objects; this will be the result of a mongo query, that's only frequencyObjects
 
@@ -47,7 +72,7 @@ MongoClient.connect("mongodb://localhost:27017/tracedcrawler", function(err, db)
       process.kill();
     }
     // put function calls here
-    findSites(articles);
+    getFrequencyObjectBySite(articles);
   })
 
 
